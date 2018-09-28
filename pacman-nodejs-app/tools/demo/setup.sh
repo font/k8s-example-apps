f9f53a8868e2df340fc476b4d64423816907dcf3
Ivan Font <ifont@redhat.com>
2018-09-27 22:06:05 -0700

    Add scripts to set up demo and run stress tests

---
+++ pacman-nodejs-app/tools/demo/setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -x
+kubectl patch federatednamespaceplacement pacman -p \
+    '{"spec":{"clusternames": ["gke-us-west1", "az-us-central1"]}}'
+set +x
+updatedns -t gke-us-west1 -t az-us-central1
---
+++ pacman-nodejs-app/tools/stress/test.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+TEST_CLUSTERS="aws-us-east1 az-us-central1"
+LOOP_INTERVAL=10 # in minutes
+count=0
+
+while true; do
+
+    echo -e "count ${count}"
+    # Migrate from Azure to AWS and back
+    for i in ${TEST_CLUSTERS}; do
+        echo "---------- ${i} -----------"
+        set -x
+        kubectl patch federatednamespaceplacement pacman -p \
+            "{\"spec\":{\"clusternames\": [\"gke-us-west1\", \"az-us-central1\", \"aws-us-east1\"]}}"
+        set +x
+        time updatedns -t gke-us-west1 -t ${i}
+        set -x
+        kubectl patch federatednamespaceplacement pacman -p \
+            "{\"spec\":{\"clusternames\": [\"gke-us-west1\", \"${i}\"]}}"
+        set +x
+    done
+    echo -e "\n\n\n"
+    sleep ${LOOP_INTERVAL}m
+    (( count += 1 ))
+done
